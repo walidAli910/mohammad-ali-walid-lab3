765888e4569159b03b792dbf6464284d4ef23651
Walid <Mohammad.Kh.Walid@utcluj.didatec.ro>
2021-10-24 22:00:37 +0300

    all task

---
+++ PizzaMachine.cs
@@ -0,0 +1,133 @@
+﻿
+using System;
+using System.ComponentModel;
+using System.Windows.Threading;
+
+namespace mohammad_ali_walid_lab3
+{
+    class PizzaMachine : Component
+    {
+        private PizzaType mIngredients;
+        public PizzaType Ingredients
+        {
+            get
+            {
+                return mIngredients;
+            }
+            set
+            {
+                mIngredients = value;
+            }
+        }
+        private System.Collections.ArrayList mPizzas = new System.Collections.ArrayList();
+        public Pizza this[int Index]
+        {
+            get
+            {
+                return (Pizza)mPizzas[Index];
+            }
+            set
+            {
+                mPizzas[Index] = value;
+            }
+        }
+        public delegate void PizzaCompleteDelegate();
+        public event PizzaCompleteDelegate PizzaComplete;
+
+        DispatcherTimer pizzaBakeTimer;
+        private void InitializeComponent()
+        {
+            this.pizzaBakeTimer = new DispatcherTimer();
+            this.pizzaBakeTimer.Tick += new System.EventHandler(this.pizzaBakeTimer_Tick);
+        }
+        public PizzaMachine()
+        {
+            InitializeComponent();
+        }
+        private void pizzaBakeTimer_Tick(object sender, EventArgs e)
+        {
+            Pizza aPizza = new Pizza(this.Ingredients);
+            mPizzas.Add(aPizza);
+            PizzaComplete();
+        }
+        public bool Enabled
+        {
+            set
+            {
+                pizzaBakeTimer.IsEnabled = value;
+            }
+        }
+        public int Interval
+        {
+            set
+            {
+                pizzaBakeTimer.Interval = new TimeSpan(0, 0, value);
+            }
+        }
+        public void MakePizzas(PizzaType dIngredients)
+        {
+            Ingredients = dIngredients;
+            switch (Ingredients)
+            {
+                case PizzaType.Canibale: Interval = 3; break;
+                case PizzaType.Margherita: Interval = 2; break;
+                case PizzaType.Pepperoni: Interval = 5; break;
+                case PizzaType.Quattro_Stagioni: Interval = 7; break;
+                case PizzaType.Veggie: Interval = 4; break;
+            }
+            pizzaBakeTimer.Start();
+        }
+    }
+
+
+    enum PizzaType
+    {
+        Margherita,
+        Pepperoni,
+        Veggie,
+        Quattro_Stagioni,
+        Canibale
+    }
+    class Pizza
+    {
+        private PizzaType mIngredients;
+        public PizzaType Ingredients
+        {
+            get
+            {
+                return mIngredients;
+            }
+            set
+            {
+                mIngredients = value;
+            }
+        }
+        private float mPrice = .50F;
+        private float Price
+        {
+            get
+            {
+                return mPrice;
+            }
+            set
+            {
+                mPrice = value;
+            }
+        }
+        private readonly DateTime mTimeOfCreation;
+        public DateTime TimeOfCreation
+        {
+            get
+            {
+                return mTimeOfCreation;
+            }
+        }
+        public Pizza(PizzaType aIngredients)
+        {
+            mTimeOfCreation = DateTime.Now;
+            mIngredients = aIngredients;
+        }
+    }
+}
+
+
